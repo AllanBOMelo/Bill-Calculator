d8a735cbd322f1e8071e93239351bf64d65c4899
Allan Barbosa <allanbarbosaom@gmail.com>
2023-06-07 13:53:29 -0300

    Header for visual improvement

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+function Header () {
+    return (
+        <div className={styles.headerContainer}> 
+            <h2>BILL MAKER</h2>
+            <div></div>
+        </div>
+    );
+};
+
+export default Header;
