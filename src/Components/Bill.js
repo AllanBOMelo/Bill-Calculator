c7a1e54cd04a16e11f29c207e239f444f136e8e6
Allan Barbosa <allanbarbosaom@gmail.com>
2023-06-07 09:29:34 -0300

    Bill per client added on DOM

--- src/Components/Bill.js
+++ src/Components/Bill.js
@@ -3,7 +3,31 @@ function Bill (props) {
     let clients = props.array
     
     function billGenerator () {
-        
+
+        const mainDiv = document.getElementById('billGenerator')
+        clear()   
+
+        clients.forEach(element => {
+            let div = document.createElement('div')
+
+            let p = document.createElement('p')
+            p.textContent = element.name + ': '
+
+            let label = document.createElement('label')
+            label.textContent = '$ ' + element.value
+
+            div.appendChild(p)
+            div.appendChild(label)
+
+            mainDiv.appendChild(div)
+        });
+    }
+
+    function clear () {
+
+        const mainDiv = document.getElementById('billGenerator')
+
+        mainDiv.innerHTML = '';
     }
 
     return (
