81d4251fd0371622917f1eeaf922498c7ca2aad3
Allan Barbosa <allanbarbosaom@gmail.com>
2023-06-07 13:53:48 -0300

    Font and Header imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Program from './Components/Program';
+import Header from './Components/Header';
 
 
 function App() {
   return (
     <div className='app'>
+        <Header />
         <Program />
     </div>
   );
